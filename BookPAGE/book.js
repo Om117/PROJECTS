637cc17b3cb0f08c16a1d7e2d428207c4e910d0d
Om Jadav <omjadav04@gmail.com>
2023-04-18 17:22:48 +0530

    updated

--- BookPAGE/book.js
+++ BookPAGE/book.js
@@ -47,3 +47,31 @@ var swiper = new Swiper(".books-slider", {
       },
     },
   });
+
+  var swiper = new Swiper(".featured-slider", {
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+     delay:4500,
+     disableOnInteraction:false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+     breakpoints: {
+       0: {
+         slidesPerView: 1,
+       },
+       450: {
+         slidesPerView: 2,
+       },
+       768: {
+         slidesPerView: 3,
+ 
+       },
+       1024: {
+         slidesPerView: 4,
+       },
+     },
+   });
